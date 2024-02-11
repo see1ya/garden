2f85ae727fa9b54d2f2857822a02431a590c5245
s-watanabe <driver100200@gmail.com>
2024-02-11 22:01:46 +0900

    assetを追加、階層も修正

---
+++ lib/Util/image.dart
@@ -0,0 +1,6 @@
+const String cookActiveIcon = "assets/home/cookActiveIcon.png";
+const String craftIcon = "assets/home/craftIcon.png";
+const String drawIcon = "assets/home/drawIcon.png";
+const String experimentIcon = "assets/home/experimentIcon.png";
+const String playIcon = "assets/home/playIcon.png";
+const String searchIcon = "assets/home/searchIcon.png";
